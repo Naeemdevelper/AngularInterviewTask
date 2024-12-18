ed7e4a38dc7fcf73651255e380dc472d46d54a19
Naeemdevelper <89269495+Naeemdevelper@users.noreply.github.com>
2024-12-18 16:26:54 +0000

    Dist create

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiSport:'https://www.thesportsdb.com/api/v1/json/40130162/all_sports.php',
+  apiLeague:'https://www.thesportsdb.com/api/v1/json/40130162/all_leagues.php',
+  apiTeams:'https://www.thesportsdb.com/api/v1/json/40130162/lookup_all_teams.php'
 };
